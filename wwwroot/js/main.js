ff0481ed277b3a398ad59415b5225ea66e731c8b
Petr Zabransky <info@petrzabransky.cz>
2022-02-21 21:19:24 +0100

    Zrušení Smooth scroll z JS

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -1,24 +1,7 @@
-window.onload = function() {
-    smoothScroll();
+window.onload = function () {
     navMenu();
 }
 
-// Smooth scrool
-function smoothScroll() {
-    const links = document.querySelectorAll('a[href^="#"]');
-    for (let i = 0; i < links.length; i++) links[i].onclick = function() {
-        const target = document.querySelector(this.attributes.href.nodeValue),
-            targetY = target.offsetTop;
-        var speed, dir, step, dist, screenY,
-            anim = setInterval((function() {
-                screenY = document.documentElement.scrollTop + document.body.scrollTop,
-                    dir = screenY <= targetY ? 1 : -1,
-                    dist = Math.abs(screenY - targetY),
-                    (speed = dist / 10) < 1 && (speed = 1), step = dir * speed, dist < 1 ? clearInterval(anim) : scrollBy(0, step)
-            }), 10);
-        return !1
-    }
-}
 
 // Navigation menu collapse
 function navMenu() {
@@ -26,7 +9,7 @@ function navMenu() {
     const close = document.querySelector(".nav__close");
     const menu = document.querySelector(".nav__menu");
 
-    hamb.addEventListener("click", function() {
+    hamb.addEventListener("click", function () {
         if (menu.classList.contains("nav__menu--active")) {
             menu.classList.remove("nav__menu--active");
             hamb.src = "/img/ikona/hamburger.svg";
